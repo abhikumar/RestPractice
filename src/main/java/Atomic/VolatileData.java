c6c728bafb1adcc1db6833aec714d96f74bfd81e
akumar18 <abhikiet39@gmail.com>
2017-06-29 16:11:04 +0530

    atomic and volatile.

---
+++ src/main/java/Atomic/VolatileData.java
@@ -0,0 +1,14 @@
+package Atomic;
+
+public class VolatileData {
+	
+	private volatile int counter = 0;
+	
+	public int getCounter() {
+		return counter;
+	}
+	
+	public void increaseCounter() {
+		++counter;
+	}
+}
